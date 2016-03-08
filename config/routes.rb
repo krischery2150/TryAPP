4012021001e39d5ac09205ffac23f9dce4b6c58d
Kris Chery <kris.chery2@gmail.com>
2016-03-07 19:39:07 -0600

    Setup strong parameters, made changes to profile method in pages controller, made changes to routes to get to a particular users profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root      'pages#index'
 
   get   'home'    =>   'pages#home'
 
-  get   'profile' =>   'pages#profile'
+  get   'user/:id' =>   'pages#profile'
 
   get   'explore' =>   'pages#explore'
 
